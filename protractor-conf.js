8b34d7eb177156a339d4cca5e289d408e789f1ff
Julie <ju.ralph@gmail.com>
2014-01-07 11:51:32 -0800

    Protractor Update (draft) - adding protractor config

---
+++ protractor-conf.js
@@ -0,0 +1,33 @@
+// A reference configuration file.
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  // The timeout for each script run on the browser. This should be longer
+  // than the maximum time your application needs to stabilize between tasks.
+  allScriptsTimeout: 11000,
+
+  // ----- What tests to run -----
+  //
+  // Spec patterns are relative to the location of this config.
+  specs: [
+    'build/docs/ptore2e/**/*.js',
+  ],
+
+  // ----- Capabilities to be passed to the webdriver instance ----
+  //
+  // For a full list of available capabilities, see
+  // https://code.google.com/p/selenium/wiki/DesiredCapabilities
+  // and
+  // https://code.google.com/p/selenium/source/browse/javascript/webdriver/capabilities.js
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  // ----- More information for your tests ----
+  //
+  // A base URL for your application under test. Calls to protractor.get()
+  // with relative paths will be prepended with this.
+  baseUrl: 'http://localhost:8000/build/docs/',
+
+  framework: 'jasmine',
+};
