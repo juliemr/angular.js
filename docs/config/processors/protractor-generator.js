764f8d63e3838eff5fa64e3588f46ec25b995207
Peter Bacon Darwin <pete@bacondarwin.com>
2014-02-12 06:02:22 +0000

    WIP remove example-copy-paste code

--- docs/config/processors/protractor-generator.js
+++ docs/config/processors/protractor-generator.js
@@ -3,52 +3,7 @@ var log = require('winston');
 var path = require('canonical-path');
 var trimIndentation = require('dgeni/lib/utils/trim-indentation');
 var code = require('dgeni/lib/utils/code');
-var templateFolder, outputFolder, commonFiles;
-
-
-function outputPath(example, fileName) {
-  return path.join(example.outputFolder, fileName);
-}
-
-function createExampleDoc(example) {
-  var exampleDoc = {
-    id: example.id,
-    docType: 'example',
-    template: path.join(templateFolder, 'index.template.html'),
-    file: example.doc.file,
-    startingLine: example.doc.startingLine,
-    example: example,
-    path: example.id,
-    outputPath: example.outputFolder + '/index.html'
-  };
-
-  // Copy in the common scripts
-  exampleDoc.scripts = _.map(commonFiles.scripts || [], function(script) { return { path: script }; });
-  exampleDoc.stylesheets = _.map(commonFiles.stylesheets || [], function(stylesheet) { return { path: stylesheet }; });
-
-  // If there is an index.html file specified then use it contents for this doc
-  // and remove it from the files property
-  if ( example.files['index.html'] ) {
-    exampleDoc.fileContents = example.files['index.html'].fileContents;
-    delete example.files['index.html'];
-  }
-  return exampleDoc;
-}
-
-function createFileDoc(example, file) {
-  var fileDoc = {
-    docType: 'example-' + file.type,
-    id: example.id + '/' + file.name,
-    template: path.join(templateFolder, 'template.' + file.type),
-    file: example.doc.file,
-    startingLine: example.doc.startingLine,
-    example: example,
-    path: file.name,
-    outputPath: outputPath(example, file.name),
-    fileContents: file.fileContents
-  };
-  return fileDoc;
-}
+var protractorFolder;
 
 
 module.exports = {
