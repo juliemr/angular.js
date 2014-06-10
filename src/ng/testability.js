d7c7adf340b2a7db5a4fda4946851bdbcf96b963
Julie <ju.ralph@gmail.com>
2014-06-10 10:24:09 -0700

    Testability WIP - add interface and tests for binding

--- src/ng/testability.js
+++ src/ng/testability.js
@@ -15,6 +15,36 @@ function $TestabilityProvider() {
       return [];
     };
 
+    /**
+     * Returns an array of elements that are two-way found via ng-model to
+     * expressions matching the input.
+     */
+    testability.findModels = function(element, expressions, opt_exactMatch) {
+
+    };
+
+    /**
+     * Shortcut for getting the location in a browser agnostic way.
+     */
+    testability.getLocation = function() {
+
+    }
+
+    /**
+     * Shortcut for navigating to a location without doing a full page reload.
+     */
+    testability.setLocation = function(path) {
+
+    }
+
+    /**
+     * Calls the callback when $timeout and $http
+     * requests are completed. Move from $browser
+     */
+    testability.notifyWhenNoOutstandingRequests = function(path, callback) {
+      
+    }
+
 
     return testability;
   }];
--- test/ng/testabilitySpec.js
+++ test/ng/testabilitySpec.js
@@ -6,18 +6,24 @@ ddescribe('$testability', function() {
   });
 
   describe('finding elements', function() {
-    var $testability, element;
-    beforeEach(inject(function(_$testability_, $compile, $rootScope) {
+    var $testability, $compile, scope, element;
+    beforeEach(inject(function(_$testability_, _$compile_, $rootScope) {
       $testability = _$testability_;
+      $compile = _$compile_;
+      scope = $rootScope.$new();
+    }));
+
+    afterEach(function() {
+      scope.$destroy();
+    });
+
+    it('should find partial bindings', function() {
       element =
           '<div>' +
           '  <span>{{name}}</span>' +
           '  <span>{{username}}</span>' +
-          '</div';
-      element = $compile(element)($rootScope);
-    }));
-
-    it('should find partial bindings', function() {
+          '</div>';
+      element = $compile(element)(scope);
       var names = $testability.findBindings(element[0], 'name');
       expect(names.length).toBe(2);
       expect(names[0]).toBe(element.find('span')[0]);
@@ -25,7 +31,37 @@ ddescribe('$testability', function() {
     });
 
     it('should find exact bindings', function() {
+      element =
+          '<div>' +
+          '  <span>{{name}}</span>' +
+          '  <span>{{username}}</span>' +
+          '</div>';
+      element = $compile(element)(scope);
       var users = $testability.findBindings(element[0], 'username', true);
+      expect(users.length).toBe(1);
+      expect(users[0]).toBe(element.find('span')[1]);
+    });
+
+    it('should find findings by class', function() {
+      element =
+          '<div>' +
+          '  <span ng-bind="name"></span>' +
+          '  <span>{{username}}</span>' +
+          '</div>';
+      element = $compile(element)(scope);
+      var users = $testability.findBindings(element[0], 'username', true);
+      expect(users.length).toBe(1);
+      expect(users[0]).toBe(element.find('span')[1]);
+    });
+
+    it('should only search within the context element', function() {
+      element =
+          '<div>' +
+          '  <div><span>{{name}}</span></div>' +
+          '  <div><span>{{name}}</span></div>' +
+          '</div>';
+      element = $compile(element)(scope);
+      var users = $testability.findBindings(element.find('div')[0], 'username', true);
       expect(users.length).toBe(1);
       expect(users[0]).toBe(element.find('span')[1]);
     });
